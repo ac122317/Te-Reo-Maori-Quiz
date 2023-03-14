cd5aad6547d1de5d1f9de4ea00ddf31c227210e3
ac122317 <ac122317@avcol.school.nz>
2023-03-15 12:07:34 +1300

    I have started my program. I displayed a short introduction about the quiz and asked the user for their name.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String name;
+
+            //I am welcome the using to the quiz and asking for their name and assigning it to the name variable.
+
+            Console.WriteLine("\t\t\tWelcome to my Te Reo Maori Quiz.\nThis is a multiple choice quiz with three different levels, Easy, Medium and Hard. Good luck!");
+
+            Console.Write("\nPlease enter your name to begin: ");
+            name = Console.ReadLine();
+
+            Console.WriteLine("\nHello " + name + ", please choose your level: Easy, Medium or Hard (please type E for easy, M for Medium or H for Hard).");
+
+
         }
     }
 }
