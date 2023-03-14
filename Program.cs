0d8fc374b6708f991a07ac064b7c04ae39094fe1
ac122317 <ac122317@avcol.school.nz>
2023-03-15 12:53:01 +1300

    Added small changes such as comments where the beginning and end of methods are and I added Console.Clear before the level methods started in the if statements.

--- Program.cs
+++ Program.cs
@@ -2,49 +2,58 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Beginning of playing method.
         {
             playing();
             
-            static void playing()
+            static void playing() //Beginning of playing Method.
             {
                 String name, level;
                 
-                //I am welcoming the user to the quiz and asking for their name and assigning it to the name variable.
-
-                Console.WriteLine("\t\t\tWelcome to my Te Reo Maori Quiz.\nThis is a multiple choice quiz with three different levels, Easy, Medium and Hard. Good luck!");
+                Console.WriteLine("\t\t\tWelcome to my Te Reo Maori Quiz.\nThis is a multiple choice quiz with three different levels, Easy, Medium and Hard. Each level will have 10 different questions and for each correct question you will be awarded 1 point. Good luck!"); 
 
                 Console.Write("\nPlease enter your name to begin: ");
-                name = Console.ReadLine();
+                name = Console.ReadLine(); //I am welcoming the user to the quiz and asking for their name and assigning it to the name variable.
 
                 Console.WriteLine("\nHello " + name + ", please choose your level: Easy, Medium or Hard (please type E for easy, M for Medium or H for Hard).");
-                level = Console.ReadLine().ToUpper();
+                level = Console.ReadLine().ToUpper(); //Asks user what level they would like to play and takes input from them to decide this.
+                Convert.ToBoolean(level)
+                while (level )
+                {
+                    if (level == "E")
+                    {
+                        Console.Clear();
+                        level1();
+                    }
+                    else if (level == "M")
+                    {
+                        Console.Clear();
+                        level2();
+                    }
+                    else if (level == "H")
+                    {
+                        Console.Clear();
+                        level3();
+                    }
+                    else
+                    {
 
-                if (level == "E")
-                {
-                    level1();
+                    }
                 }
-                else if (level == "M")
-                {
-                    level2();
-                }
-                else if (level == "H")
-                {
-                    level3();
-                }
-            }
+            } //End of playing method.
+            
             static void level1()
             {
-                Console.WriteLine("Welcome to the Easy level.");
+                Console.WriteLine("\nWelcome to the Easy level.");
             }
             static void level2()
             {
-                Console.WriteLine("Welcome to the Medium level.");
+                Console.WriteLine("\nWelcome to the Medium level.");
             }
             static void level3()
             {
-                Console.WriteLine("Welcome to the Hard level.");
+                Console.WriteLine("\nWelcome to the Hard level.");
             }
-        }
+        } //End of main method.
     }
 }
