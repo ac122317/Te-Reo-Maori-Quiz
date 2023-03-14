e703e019d425f64cd8cdb147c7c6c4952caa8e63
ac122317 <ac122317@avcol.school.nz>
2023-03-15 12:31:08 +1300

    I have corrected some spelling mistakes, created methods for playing and the 3 levels, but have not done data validation yet.

--- Program.cs
+++ Program.cs
@@ -4,18 +4,47 @@
     {
         static void Main(string[] args)
         {
-            String name;
+            playing();
+            
+            static void playing()
+            {
+                String name, level;
+                
+                //I am welcoming the user to the quiz and asking for their name and assigning it to the name variable.
 
-            //I am welcome the using to the quiz and asking for their name and assigning it to the name variable.
+                Console.WriteLine("\t\t\tWelcome to my Te Reo Maori Quiz.\nThis is a multiple choice quiz with three different levels, Easy, Medium and Hard. Good luck!");
 
-            Console.WriteLine("\t\t\tWelcome to my Te Reo Maori Quiz.\nThis is a multiple choice quiz with three different levels, Easy, Medium and Hard. Good luck!");
+                Console.Write("\nPlease enter your name to begin: ");
+                name = Console.ReadLine();
 
-            Console.Write("\nPlease enter your name to begin: ");
-            name = Console.ReadLine();
+                Console.WriteLine("\nHello " + name + ", please choose your level: Easy, Medium or Hard (please type E for easy, M for Medium or H for Hard).");
+                level = Console.ReadLine().ToUpper();
 
-            Console.WriteLine("\nHello " + name + ", please choose your level: Easy, Medium or Hard (please type E for easy, M for Medium or H for Hard).");
-
-
+                if (level == "E")
+                {
+                    level1();
+                }
+                else if (level == "M")
+                {
+                    level2();
+                }
+                else if (level == "H")
+                {
+                    level3();
+                }
+            }
+            static void level1()
+            {
+                Console.WriteLine("Welcome to the Easy level.");
+            }
+            static void level2()
+            {
+                Console.WriteLine("Welcome to the Medium level.");
+            }
+            static void level3()
+            {
+                Console.WriteLine("Welcome to the Hard level.");
+            }
         }
     }
 }
