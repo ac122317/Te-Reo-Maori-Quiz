fb790997ca78bb96164009a03963cca603cfe64b
jahma <jahma@JAHMAYNS-LAPTOP>
2023-03-15 11:43:18 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Te_Reo_Maori_Quiz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
