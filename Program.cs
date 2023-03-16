63a7e908f84ee240295702d5a6026c8317fa38d5
ac122317 <ac122317@avcol.school.nz>
2023-03-16 13:06:46 +1300

    I created a static integer called points in the Program class so a running score can be utilized in each of my level methods. As it isn't inside the playing method, the entire code can use this integer making it more efficient than having to reference it a lot.

--- Program.cs
+++ Program.cs
@@ -8,32 +8,32 @@
 
             static void playing() //Beginning of playing Method.
             {
-                String name, level;
+                String playerName, selectedLevel;
                 bool valid = false;
 
                 Console.WriteLine("\t\t\tWelcome to my Te Reo Maori Quiz.\nThis is a multiple choice quiz with three different levels, Easy, Medium and Hard. Each level will have 10 different questions and for each correct question you will be awarded 1 point. Good luck!");
 
                 Console.Write("\nPlease enter your name to begin: ");
-                name = Console.ReadLine(); //I am welcoming the user to the quiz and asking for their name and assigning it to the name variable.
+                playerName = Console.ReadLine(); //I am welcoming the user to the quiz and asking for their name and assigning it to the name variable.
 
-                Console.WriteLine("\nHello " + name + ", please choose your level: Easy, Medium or Hard (please type E for easy, M for Medium or H for Hard).");
+                Console.WriteLine("\nHello " + playerName + ", please choose your level: Easy, Medium or Hard (please type E for easy, M for Medium or H for Hard).");
 
                 do
                 {
-                    level = Console.ReadLine().ToUpper(); //Asks user what level they would like to play and takes input from them to decide this.
-                    if (level == "E")
+                    selectedLevel = Console.ReadLine().ToUpper(); //Asks user what level they would like to play and takes input from them to decide this.
+                    if (selectedLevel == "E")
                     {
                         Console.Clear();
                         level1();
                         valid = true;
                     }
-                    else if (level == "M")
+                    else if (selectedLevel == "M")
                     {
                         Console.Clear();
                         level2();
                         valid = true;
                     }
-                    else if (level == "H")
+                    else if (selectedLevel == "H")
                     {
                         Console.Clear();
                         level3();
@@ -44,7 +44,7 @@
                         Console.Write("Please enter a valid option: ");
                         valid = false;
                     }
-                } while (valid != true);
+                } while (valid != true); //This do while loop validates if the input entered by the user is accepted into the program, if it doesn't an error message will display and prompt the user to enter a valid option. The do loop ensures the input is proccessed at least once before checking if there is an error.
             }
         } //End of playing method.
 
