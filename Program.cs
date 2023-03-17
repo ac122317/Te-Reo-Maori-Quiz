27c25bb44e0d16a722098aea524db2a239fe0a71
ac122317 <ac122317@avcol.school.nz>
2023-03-17 13:15:29 +1300

    I have added data validation for the playerName string to ensure that the user enters at least 1 character as their name. I utilized a do while statement with an if else statement inside. I may come back in the future to change this to make it more concise.

--- Program.cs
+++ Program.cs
@@ -11,12 +11,25 @@
             {
                 String playerName, selectedLevel;
                 bool valid = false;
-                
-
+                bool playerNameCheck = true;
                 Console.WriteLine("\t\t\tWelcome to my Te Reo Maori Quiz.\nThis is a multiple choice quiz with three different levels, Easy, Medium and Hard. Each level will have 10 different questions and for each correct question you will be awarded 1 point. Good luck!");
 
-                Console.Write("\nPlease enter your name to begin: ");
-                playerName = Console.ReadLine(); //I am welcoming the user to the quiz and asking for their name and assigning it to the name variable.
+                Console.Write("\nPlease enter your name to begin: "); //I am welcoming the user to the quiz and asking for their name.
+
+                do //Creating a do loop to ensure the name check is done at least once.
+                {
+                    playerName = Console.ReadLine(); // I am now assigning their input to the name variable.
+
+                    if (string.IsNullOrEmpty(playerName)) //This if statement checks if the user doesn't input anything and just presses enter. If so, they will be prompted to enter a name with at least 1 character. If they follow the instructions the loop will be broken, otherwise it will loop until the condition is met.
+                    {
+                        Console.Write("Please enter a name with at least 1 character: ");
+                        playerNameCheck = false;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                } while (playerNameCheck != true); //Ensures the loop repeats if the playerNameCheck does not equal true.
 
                 Console.WriteLine("\nHello " + playerName + ", please choose your level: Easy, Medium or Hard (please type E for easy, M for Medium or H for Hard).");
 
@@ -24,7 +37,7 @@
                 {
                     selectedLevel = Console.ReadLine().ToUpper(); //Asks user what level they would like to play and takes input from them to decide this.
 
-                    switch (selectedLevel) //This switch will check the input from the user and validate whether it is accepted, if accepted the user will be sent to the corresponding level method based on their selection.
+                    switch (selectedLevel) //This switch will check the input from the user and validate whether it is accepted according to each case. If accepted the user will be sent to the corresponding level method based on their selection, otherwise an error will display and loop back to take an appropriate input.
                     {
                         case "E":
                             Console.Clear();
@@ -57,16 +70,20 @@
         {
             points += 0;
             Console.WriteLine("Welcome to the Easy level.");
+            Console.WriteLine("Your points are currently: " +points);
         } //End of level1 method.
         static void level2()
         {
             points += 0;
             Console.WriteLine("Welcome to the Medium level.");
-        }
+            Console.WriteLine("Your points are currently: " + points);
+        } //End of level2 method.
         static void level3()
         {
             points += 0;
             Console.WriteLine("Welcome to the Hard level.");
-        }
+            Console.WriteLine("Your points are currently: " + points);
+        } //End of level3 method.
+
     } //End of main method.
 }
